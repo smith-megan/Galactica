50218778916b6aafd7208e9d124950e16ff5d4e7
Megan <smith.megan.design@gmail.com>
2024-06-12 14:18:31 -0700

    generate buttons based on current date

--- galactica/src/App.tsx
+++ galactica/src/App.tsx
@@ -1,20 +1,30 @@
-import React from "react"
-import {
-  format,
-  formatDistance,
-  formatRelative,
-  subDays,
-  getDaysInMonth,
-} from "date-fns"
+import { useEffect, useState } from "react"
+import { format, getDaysInMonth } from "date-fns"
 
 function App() {
-  let date = format(new Date(2014, 5, 11), "MMM/dd/yyyy")
-  let button = getDaysInMonth(date)
+  const [buttonArray, setButtonArray] = useState([305, 90])
+
+  let date = format(new Date(2014, 6, 11), "MMM/dd/yyyy")
+  let dayCount = getDaysInMonth(date)
+
+  let renderButtons = (dayCount: number) => {
+    let newArray = []
+    for (let i = 1; i < dayCount + 1; i++) {
+      newArray.push(i)
+    }
+    setButtonArray(newArray)
+  }
+
+  useEffect(() => {
+    renderButtons(dayCount)
+  }, [date])
 
   return (
     <div className="App">
       <div>{date}</div>
-      {button}
+      {buttonArray.map((item: number) => {
+        return <div>{item}</div>
+      })}
     </div>
   )
 }
